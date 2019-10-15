0ea02aa7c22dbf106e2c0c317dc7add86aaf2a08
Srinivasan Dodda <srinivasan.dodda@affinitas.de>
2019-10-15 11:30:30 +0200

    removed iOS 13 REFERENCE

--- UnsplashPhotoPicker/UnsplashPhotoPicker/Classes/Utils/UIColor+PhotoPicker.swift
+++ UnsplashPhotoPicker/UnsplashPhotoPicker/Classes/Utils/UIColor+PhotoPicker.swift
@@ -10,15 +10,12 @@ import UIKit
 
 struct PhotoPickerColors {
     var background: UIColor {
-        if #available(iOS 13.0, *) { return .systemBackground }
         return .white
     }
     var titleLabel: UIColor {
-        if #available(iOS 13.0, *) { return .label }
         return .black
     }
     var subtitleLabel: UIColor {
-        if #available(iOS 13.0, *) { return .secondaryLabel }
         return .gray
     }
 }
